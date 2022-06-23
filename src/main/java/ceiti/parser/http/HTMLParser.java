86ca8bd31537f7179093a2cece718b4db186aa95
Caffadras <senya.dubik@gmail.com>
2022-06-23 22:21:40 +0300

    Added exam parsing. Now exam grades are parsed from the html document, but they are not displayed in the UI yet.

--- src/main/java/ceiti/parser/http/HTMLParser.java
+++ src/main/java/ceiti/parser/http/HTMLParser.java
@@ -13,11 +13,67 @@ import java.util.regex.Pattern;
 
 
 public class HTMLParser {
+
+    private static int getYearOfStudy(@NotNull Document htmlDocument) {
+        String yearOfStudy = null;
+        try{
+            Element personalInfoTable = htmlDocument.getElementsByAttributeValue("id","date-personale").first();
+            for (Element row : personalInfoTable.getElementsByTag("tr")) {
+                if(row.getElementsByTag("th").first().text().equals("Anul de studii")){
+                   yearOfStudy = row.getElementsByTag("td").first().text();
+                    break;
+                }
+            }
+        } catch(Exception e){
+            //todo handle exception?
+        }
+        if (yearOfStudy != null){
+            return switch (yearOfStudy) {
+                case "I" -> 1;
+                case "II" -> 2;
+                case "III" -> 3;
+                case "IV" -> 4;
+                default -> throw new IllegalArgumentException("Unknown year of study!");
+            };
+        }
+
+        throw new IllegalArgumentException("Unknown year of study!");
+    }
+
+    private static List<String> getExams(@NotNull Document htmlDocument, int yearOfStudy){
+        String firstSemester = "collapse" + ((yearOfStudy * 2)-2);
+        String secondSemester = "collapse" + ((yearOfStudy * 2)-1);
+        List<String> exams = new ArrayList<>();
+
+        getExamsBySemester(htmlDocument, firstSemester, exams);
+
+        getExamsBySemester(htmlDocument, secondSemester, exams);
+
+        return exams;
+    }
+
+    private static void getExamsBySemester(@NotNull Document htmlDocument, String semester, List<String> exams) {
+        Element secondSemesterExamsTable = htmlDocument.getElementById(semester);
+        for (Element row : secondSemesterExamsTable.getElementsByTag("tr")) {
+            Elements exam = row.getElementsByTag("td");
+            if (exam.size() == 2){
+                String examName = exam.first().text();
+                String examNote = exam.last().text();
+                exams.add(examName + ": " + examNote);
+            }
+        }
+    }
+
     public static List<Subject> parseSubjects(@NotNull Document htmlDocument, int semester){
         List<Subject> subjects = new ArrayList<>();
 
-        Elements elements =  htmlDocument.getElementsByAttributeValue("aria-labelledby", "headin3g2");
+        int yearOfStudy = getYearOfStudy(htmlDocument);
+        List<String> exams = getExams(htmlDocument, yearOfStudy);
+        for (String exam : exams) {
+            System.out.println(exam);
+        }
 
+        Elements elements =  htmlDocument.getElementsByAttributeValue("aria-labelledby", "headin3g2");
         for (Element element : elements){
             Elements table = element.getElementsByTag("tr");
             for (Element row : table){
